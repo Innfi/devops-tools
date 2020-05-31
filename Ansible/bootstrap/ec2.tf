3b5ae88ebd0bc3efd69ec1336a6ce59d6e53ba38
innfi <innfi.world@gmail.com>
2020-05-31 23:03:13 +0900

    add ansible target instances

---
+++ Ansible/bootstrap/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "ansible-target1" {
+    ami = "ami-00edfb46b107f643c"
+    instance_type = "t2.micro"
+}
+
+resource "aws_instance" "ansible-target2" {
+    ami = "ami-00edfb46b107f643c"
+    instance_type = "t2.micro"
+}
