26f3e3f1f7e3c6aea2567b3cbd9ecaf651cfebe0
innfi <innfi.world@gmail.com>
2022-12-27 08:04:54 +0900

    eksctl-alb-controller: simple command only

--- K8s/eksctl-alb-controller/create-policy.sh
+++ K8s/eksctl-alb-controller/create-policy.sh
@@ -2,5 +2,3 @@
 aws iam create-policy \
 --policy-name AWSLoadBalancerControllerIAMPolicy \
 --policy-document file://iam_policy.json
-
-#arn:aws:iam::525017980305:policy/AWSLoadBalancerControllerIAMPolicy
--- K8s/eksctl-alb-controller/eksctl-serviceaccount.sh
+++ K8s/eksctl-alb-controller/eksctl-serviceaccount.sh
@@ -3,6 +3,6 @@ eksctl create iamserviceaccount \
 --cluster=alchemy \
 --namespace=kube-system \
 --name=aws-load-balancer-controller \
---attach-policy-arn=arn:aws:iam::525017980305:policy/AWSLoadBalancerControllerIAMPolicy \
+--attach-policy-arn=arn:aws:iam::():policy/AWSLoadBalancerControllerIAMPolicy \
 --override-existing-serviceaccounts \
 --approve 
