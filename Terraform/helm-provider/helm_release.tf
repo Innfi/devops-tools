47e320a3d6adb583c171c10b844ce7fea00aa3e5
innfi <innfi.world@gmail.com>
2023-01-02 22:16:33 +0900

    terraform: helm provider with local chart

---
+++ Terraform/helm-provider/helm_release.tf
@@ -0,0 +1,33 @@
+data "terraform_remote_state" "eks" {
+  backend = "local"
+  config = {
+    path = "../terraform.tfstate"
+  }
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
+
+provider "helm" {
+  kubernetes {
+    host = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
+}
+
+resource "helm_release" "custom_release" {
+  name = "custom_release"
+  # helm_release = "https://charts.bitnami.com/bitnami"
+
+  chart = "./chart"
+
+  values = [
+    file("${path.module}/chart/values.yaml")
+  ]
+}
