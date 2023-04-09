d9c648c7b1fe89ba82136e9a8c798c97597ca663
innfi <innfi.world@gmail.com>
2023-04-09 19:44:07 +0900

    cludwatch_alarm_rds: fix settings

--- Terraform/cloudwatch_alarm_rds/main.tf
+++ Terraform/cloudwatch_alarm_rds/main.tf
@@ -1,25 +1,22 @@
 resource "aws_cloudwatch_metric_alarm" "rds_alarm" {
-  alarm_name                = "terraform-test-foobar"
+  alarm_name                = "rds-alarm-cpu"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = 2
   threshold                 = 50
-  alarm_description         = "Request error rate has exceeded 50%"
+  alarm_description         = "CPU usage has exceeded 50%"
+  alarm_actions = [
+    var.alarm-sns-arn
+  ]
+
   insufficient_data_actions = []
 
-  metric_query {
-    id = "metric_cpu"
+  metric_name = "CPUUtilization"
+  namespace = "AWS/RDS"
+  period = 120
+  statistic = "Average"
 
-    metric {
-      metric_name = "RDSCPU"
-      namespace   = "AWS/RDS"
-      period      = 120
-      stat        = "Sum"
-      unit        = "Count"
-
-      dimensions = {
-        # DBClusterIdentifier = var.
-        DBClusterIdentifier = var.instanceid
-      }
-    }
+  dimensions = {
+    # DBClusterIdentifier = var.
+    DBClusterIdentifier = var.instanceid
   }
 }
--- Terraform/cloudwatch_alarm_rds/variables.tf
+++ Terraform/cloudwatch_alarm_rds/variables.tf
@@ -2,3 +2,8 @@ variable "instanceid" {
   description = "RDS instance id" 
   type = string
 }
+
+variable "alarm-sns-arn" {
+  description = "sns arn for alarm"
+  type = string
+}
