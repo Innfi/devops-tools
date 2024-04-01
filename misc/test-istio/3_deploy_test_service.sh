6643d43fe287180d4e4f6648eeca43b64260c319
innfi <innfi.world@gmail.com>
2024-04-01 22:16:59 +0900

    test-istio: initial commit

---
+++ misc/test-istio/3_deploy_test_service.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ ! -f ./kubernetes-manifests.yaml ]
+then
+  curl https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/main/release/kubernetes-manifests.yaml  > kubernetes-manifests.yaml
+fi
+
+kubectl apply -f ./kubernetes-manifest.yaml
