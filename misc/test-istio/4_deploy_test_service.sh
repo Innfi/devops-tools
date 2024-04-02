a3fe65047f0193d0e487c93a7daeb1c4810c0e4a
innfi <innfi.world@gmail.com>
2024-04-02 23:17:52 +0900

    test-istio: fix typo

--- misc/test-istio/4_deploy_test_service.sh
+++ misc/test-istio/4_deploy_test_service.sh
@@ -4,4 +4,4 @@ then
   curl https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/main/release/kubernetes-manifests.yaml  > kubernetes-manifests.yaml
 fi
 
-kubectl apply -f ./kubernetes-manifest.yaml
+kubectl apply -f ./kubernetes-manifests.yaml
