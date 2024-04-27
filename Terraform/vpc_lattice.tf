944a39d4c795e0bd8a533aee130775b1eb453c33
innfi <innfi.world@gmail.com>
2024-04-27 23:17:10 +0900

    terraform: vpc_lattice connecting target group to service

--- Terraform/vpc_lattice.tf
+++ Terraform/vpc_lattice.tf
@@ -1,15 +1,35 @@
 // aws_vpclattice_auth_policy
 
-// aws_vpclattice_service_network_vpc_association
-
-// aws_vpclattice_service_network
-
-// aws_vpclattice_service_network_service_association
-
-// aws_vpclattice_target_group
-
 resource "aws_vpclattice_service" "lattice_test" {
   name = "lattice_test"
   auth_type = "AWS_IAM"
   custom_domain_name = "test.com"
+}
+
+resource "aws_vpclattice_service_network" "lattice_test_network" {
+  name = "lattice_test_network"
+  auth_type = "AWS_IAM"
+}
+
+resource "aws_vpclattice_service_network_service_association" "lattice_test_network_service_association" {
+  service_identifier = aws_vpclattice_service.lattice_test.id
+  service_network_identifier = aws_vpclattice_service_network.lattice_test_network.id
+}
+
+resource "aws_vpclattice_target_group" "target_group_instance" {
+  name = "target_group_instance"
+  type = "INSTANCE" # "IP", "ALB", "LAMBDA"
+
+  config {
+    vpc_identifier = var.vpc_id
+
+    port = 443
+    protocol = "HTTPS"
+  }
+}
+
+resource "aws_vpclattice_service_network_vpc_association" "vpc_association_test" {
+  vpc_identifier             = var.vpc_id
+  service_network_identifier = aws_vpclattice_service_network.lattice_test_network.id
+  security_group_ids         = var.security_group_ids
 }
