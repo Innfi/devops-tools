76d4e4897eac413c1d9be526ddaac7e56d6ff2c6
innfi <innfi.world@gmail.com>
2024-08-24 21:56:11 +0900

    terraform: ecs task definition with vpc

--- Terraform/ecs/main.tf
+++ Terraform/ecs/main.tf
@@ -11,6 +11,19 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name = "test_vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = ["ap-northeast-2a", "ap-northeast-2b"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  enable_dns_hostnames = true
+}
+
 module "ecs_cluster" {
   source = "terraform-aws-ecs/modules/cluster"
 
@@ -31,4 +44,33 @@ module "ecs_cluster" {
   }
 }
 
-// TODO: service, task definition, relying infrastructures like vpc and alb
+module "ecs_task_definition" {
+  source = "terraform-aws-ecs/modules/service"
+
+  name = "test-service"
+  cluster_arn = module.ecs_cluster.arn
+  subnet_ids = module.vpc.private_subnets
+
+  security_group_rules = {
+    egress_all = {
+      type = "egress" 
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  runtime_platform = {
+    cpu_architecture = "ARM64"
+    operating_system_family = "LINUX"
+  }
+
+  container_definitions = {
+    test_basic_backend = {
+      image = "innfi/basic-backend:latest"
+    }
+  }
+}
+
+// TODO: service, alb
