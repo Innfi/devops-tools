d261924801dfa23d2dbbf2a19b975b2f13178515
innfi <innfi.world@gmail.com>
2024-12-26 22:46:56 +0900

    diagrams: the result may introduce a wrong impression how resources are connected :(

--- diagrams/vpc_test.py
+++ diagrams/vpc_test.py
@@ -1,8 +1,7 @@
 from diagrams import Cluster, Diagram
-from diagrams.aws.compute import ECS
+from diagrams.aws.compute import ECS, EKS, EC2
 from diagrams.aws.database import ElastiCache, RDS
-from diagrams.aws.network import ELB
-from diagrams.aws.network import Route53
+from diagrams.aws.network import ELB, Route53, VPC, PublicSubnet, PrivateSubnet
 
 with Diagram("clustered web services", show=False):
   dns = Route53("dns")
@@ -10,16 +9,26 @@ with Diagram("clustered web services", show=False):
 
   with Cluster("workload"):
     service_group = [
-      ECS("web1"),
-      ECS("web2"),
-      ECS("web3")
+      EC2("web1"),
+      EC2("web2"),
+      EC2("web3")
     ]
+
+  eks = EKS("eks cluster")
+
   with Cluster("db cluster"):
     db_primary = RDS("userdb")
     db_primary - [RDS("userdb reader")]
 
-  memcached = ElastiCache("memcached")
+  with Cluster("network"):
+    main_vpc = VPC("main")
+    subnet_pub = [PublicSubnet("pub_a"), PublicSubnet("pub_b")]
+
+    main_vpc >> subnet_pub
+
+  dns >> lb >> main_vpc
 
   dns >> lb >> service_group
   service_group >> db_primary
-  service_group >> memcached
+  dns >> lb >> eks
+
