d9f66f2f6924144f39611596e340358f8602bfde
innfi <innfi.world@gmail.com>
2025-01-22 21:24:17 +0900

    terraform: adding vpc endpoint (gateway)

--- Terraform/vpc/vpc_endpoint.tf
+++ Terraform/vpc/vpc_endpoint.tf
@@ -1,4 +1,5 @@
-resource "aws_vpc_endpoint" "test_endpoint" {
+# interface endpoint
+resource "aws_vpc_endpoint" "test_interface_endpoint" {
   vpc_id = var.vpc_id
   service_name = "test_endpoint" 
   vpc_endpoint_type = "Interface"
@@ -9,4 +10,18 @@ resource "aws_vpc_endpoint" "test_endpoint" {
 
   subnet_ids = var.subnet_ids
   private_dns_enabled = true
+}
+
+# gateway endpoint
+resource "aws_vpc_endpoint_service" "endpoint_service" {
+  acceptance_required = false
+  allowed_principals = [var.current_caller_arn]
+  gateway_load_balancer_arns = [var.loadbalancer_arn]
+}
+
+resource "aws_vpc_endpoint" "test_gateway_endpoint" {
+  service_name = aws_vpc_endpoint_service.endpoint_service.service_name
+  vpc_endpoint_type = aws_vpc_endpoint_service.endpoint_service.service_type
+  vpc_id = var.vpc_id
+  subnet_ids = [var.test_subnet_id]
 }
