b86844db9aa2a200809f2c9edff37e00ccbaa1d9
innfi <innfi.world@gmail.com>
2025-01-24 21:22:31 +0900

    terraform: s3 replication policy

--- Terraform/s3/replication.tf
+++ Terraform/s3/replication.tf
@@ -1,3 +1,52 @@
+resource "aws_iam_role" "replication_role" {
+  name = "replication_role"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect" : "Allow",
+        "Principal": {
+          "Service": "s3.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "replication_policy" {
+  name   = "repication_policy"
+  policy = jsonencode({
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetReplicationConfiguration",
+          "s3:ListBucket",
+          "s3:GetObjectVersionForReplication",
+          "s3:GetObjectVersionAcl",
+          "s3:GetObjectVersionTagging",
+        ],
+        Resource = "${var.s3_arn}"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:ReplicateObject",
+          "s3:ReplicateDelete",
+          "s3:ReplicateTags",
+        ],
+        Resource = "${var.s3_dest_arn}"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "replication" {
+  role       = aws_iam_role.replication.name
+  policy_arn = aws_iam_policy.replication_policy.arn
+}
+
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
   provider = aws.central
 
