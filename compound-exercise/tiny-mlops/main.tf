b953c07aa0839a8a361fd8c37898efaa789061a5
innfi <innfi.world@gmail.com>
2025-02-04 22:47:07 +0900

    tiny-mlops: vpc before eks

--- compound-exercise/tiny-mlops/main.tf
+++ compound-exercise/tiny-mlops/main.tf
@@ -16,3 +16,22 @@ provider "aws" {
 module "storage" {
   source = "./modules/storage"
 }
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs                 = var.azs
+  private_subnets     = [for k, v in var.azs : cidrsubnet(var.vpc_cidr, 8, k)]
+  public_subnets      = [for k, v in var.azs : cidrsubnet(var.vpc_cidr, 8, k + 4)]
+
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  enable_nat_gateway = true
+  enable_dhcp_options = true
+}
