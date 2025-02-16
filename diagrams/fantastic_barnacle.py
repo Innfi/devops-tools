0ace3c84b40e1132f340fa08d682b55f38cd2e10
innfi <innfi.world@gmail.com>
2025-02-16 20:53:51 +0900

    diagrams: wrapup for fantastic-barnacle

--- diagrams/fantastic_barnacle.py
+++ diagrams/fantastic_barnacle.py
@@ -12,47 +12,47 @@ from diagrams.onprem.monitoring import Grafana, Prometheus
 with Diagram(name="fantastic-barnacle", show=False):
   user = Client("User")
   admin = Client("Admin")
-  dockerhub = Client("Docker")
+  dockerhub = Client("Dockerhub")
 
   with Cluster("Minikube"):
     redis = Redis("Redis")
-    mysql = MySQL("MySql")
-
     argocd = ArgoCD("ArgoCd")
-    grafana = Grafana("Grafana")
-    prometheus = Prometheus("Prometheus")
 
-    with Cluster("basic-backend"):
-      bb_service = Service("Service")
-      bb_deployment = Deployment("Deployment")
-      bb_pods = [Pod("Pod1"), Pod("Pod2"), Pod("Pod3")]
+    bb_service = Service("basic-backend")
+    wh_service = Service("workload-handler")
 
-    with Cluster("workload-handler"):
-      wh_service = Service("Service")
-      wh_deployment = Deployment("Deployment")
-      wh_pods = [Pod("Pod1"), Pod("Pod2"), Pod("Pod3")]
+    with Cluster("Database"):
+      mysql = MySQL("MySql")
 
     with Cluster("Elastic"):
       kibana = Kibana("Kibana")
       es = ElasticSearch("Elasticsearch")
-  
-  bb_service - bb_deployment - bb_pods
-  wh_service - wh_deployment - wh_pods
 
-  user >> Edge(color="red", label="Request") >> bb_service
-  bb_pods >> Edge(color="red", label="Request Relayed via BullMQ") >> redis
-  wh_pods >> Edge(color="red", label="Receives Request by Subscription") >> redis
-  bb_service >> Edge(color="blue", label="Initial Response (skipped drawing deployments, pods)") >> user
+    with Cluster("Cluster Metrics"):
+      grafana = Grafana("Grafana")
+      prometheus = Prometheus("Prometheus")
 
-  wh_pods >> Edge(color="darkblue", label="Response") >> redis
-  bb_pods >> Edge(color="darkblue", label="Receives Response by Subscription") >> redis
+
+  user >> Edge(color="red", label="") >> bb_service
+  bb_service >> Edge(color="red", label="") >> redis
+  wh_service >> Edge(color="red", label="") >> redis
+
+
+  wh_service >> Edge(color="magenta", label="") >> mysql
 
   kibana - es
 
-  bb_pods - Edge(color="darkgreen") - es
-  wh_pods - Edge(color="darkgreen") - es
+  bb_service - Edge(color="darkgreen") - es
+  wh_service - Edge(color="darkgreen") - es
+
+  bb_service - Edge(color="darkgreen") - prometheus
+  bb_service - Edge(color="darkgreen") - grafana
+
+  wh_service - Edge(color="darkgreen") - prometheus
+  wh_service - Edge(color="darkgreen") - grafana
 
   admin - Edge(color="darkgreen") - kibana
-  admin - Edge(color="purple", label="Invoke Deploy") - argocd
-  argocd - Edge(color="purple") - bb_deployment
-  argocd - Edge(color="purple") - wh_deployment
+  admin - Edge(color="purple", label="") - argocd
+  argocd - Edge(color="purple", label="") - dockerhub
+  argocd - Edge(color="purple") - bb_service
+  argocd - Edge(color="purple") - wh_service
