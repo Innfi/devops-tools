438c034c3d0fd2f4a765d128802ee49f5c1cff57
innfi <innfi.world@gmail.com>
2025-02-21 22:37:35 +0900

    remove duplicates, reorganizing projects

--- compound-exercise/istio-handson/0_download.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.24.3 TARGET_ARCH=x86_64 sh -
--- compound-exercise/istio-handson/1_install_istio.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-istioctl install -f samples/bookinfo/demo-profile-no-gateways.yaml -y
-kubectl label namespace default istio-injection=enabled
