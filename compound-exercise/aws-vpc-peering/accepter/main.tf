fe1fee4a7bb458fd2779121d4c08c357529035c6
innfi <innfi.world@gmail.com>
2025-02-25 22:37:31 +0900

    aws-vpc-peering: setting up rds

--- compound-exercise/aws-vpc-peering/accepter/main.tf
+++ compound-exercise/aws-vpc-peering/accepter/main.tf
@@ -31,3 +31,22 @@ module "security-group-baseline" {
 
   vpc_id = module.vpc.vpc_id
 }
+
+module "rds" {
+  source = "terraform-aws-modules/rds/aws"
+
+  identifier = "test-rds"
+  engine = "mysql"
+  engine_version = "5.8"
+  instance_class = "db.t2.micro"
+  allocated_storage = 20
+
+  db_name = var.db_name
+  username = var.db_username
+  port = var.db_port
+
+  vpc_security_group_ids = [module.security-group-baseline.security_group_output.id]
+
+  create_db_subnet_group = false
+  subnet_ids = module.vpc.private_subnets[*].id
+}
