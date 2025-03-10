167123a0533ec1792cac510fae9e111f960c77f6
innfi <innfi.world@gmail.com>
2025-03-10 23:46:56 +0900

    eks-istio-multi: setting up eks

--- compound-exercise/eks-istio-multi/main.tf
+++ compound-exercise/eks-istio-multi/main.tf
@@ -10,3 +10,46 @@ terraform {
 provider "aws" {
   region = "ap-northeast-2"
 }
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "accepter"
+  cidr = var.vpc_cidr
+
+  azs = var.azs
+  public_subnets = var.public_subnets
+  private_subnets = var.private_subnets
+
+  enable_dns_hostnames = true
+  enable_flow_log = false
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  map_public_ip_on_launch = true
+
+  tags = {
+    "OrderBy" = "Innfi"
+  }
+}
+
+module "eks" {
+  source = "terraform-aws-module/eks/aws"
+  version = "~> 20.31"
+
+  cluster_name = "alpha"
+  cluster_version = "1.31"
+
+  cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  cluster_addons = {
+    coredns = {}
+    kube-proxy = {}
+  }
+}
+
