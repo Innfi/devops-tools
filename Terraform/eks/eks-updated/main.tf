8f272f293f0bd01486fcb622ad4b16613f800a57
innfi <innfi.world@gmail.com>
2025-03-24 22:10:39 +0900

    terraform: update basic usage for eks

---
+++ Terraform/eks/eks-updated/main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  shared_credentials_files = var.shared_credentials_files
+  profile = var.profile
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "vpc"
+  cidr = var.vpc_cidr
+  azs = var.azs
+  public_subnets = var.public_subnets
+  private_subnets = var.private_subnets
+  enable_dns_hostnames = true
+  enable_flow_log = false
+  enable_nat_gateway = true 
+  single_nat_gateway = true 
+  map_public_ip_on_launch = true
+}
+
+#eks requires security groups, for the cluster and the node group
+
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+  version = "~>20.31"
+
+  cluster_name = var.cluster_name
+  cluster_version = var.cluster_version
+  vpc_id = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_cluster_creator_admin_permissions = true 
+
+  cluster_endpoint_public_access = true 
+  cluster_endpoint_public_access_cidrs = var.cluster_endpoint_public_access_cidrs
+
+  cluster_security_group_id = var.cluster_security_group_id
+
+  eks_managed_node_groups = {
+    nodes = {
+      instance_type = var.node_group_instance_types
+      min_size = var.node_group_min_size
+      max_size = var.node_group_max_size
+      desired_size = var.node_group_desired_size
+
+      subnets = module.vpc.private_subnets
+      vpc_security_group_ids = var.node_group_security_ids
+    }
+  }
+
+  cluster_addons = {
+    coredns = {}
+    kube_proxy = {}
+    vpc-cni = {}
+    aws-ebs-csi-driver = {}
+  }
+}
--- Terraform/eks/modules/eks/outputs.tf
+++
@@ -1,6 +0,0 @@
-# Output variable definitions
-
-# output "eks_cluster_id" {
-#   description = "eks cluster id"
-#   value = aws_eks_cluster.test-eks-cluster.id
-# }
