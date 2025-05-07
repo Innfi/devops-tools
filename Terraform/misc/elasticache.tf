7286cfc48a7569d3a407254ba7b2947e51c62f0b
innfi <innfi.world@gmail.com>
2025-05-07 23:07:41 +0900

    terraform: elasticache redis revisited

--- Terraform/misc/elasticache.tf
+++ Terraform/misc/elasticache.tf
@@ -1,8 +1,36 @@
-resource "aws_elasticache_cluster" "test-redis" {
-    cluster_id = "test-redis" 
-    engine = "redis" 
-    node_type = "cache.m2.small" 
-    num_cache_nodes = 1 
-    parameter_group_name = "default.redis3.2" 
-    port = 6379
+resource "aws_elasticache_subnet_group" "redis" {
+  name       = "redis-subnet-group"
+  subnet_ids = var.private_subnet_ids
+  description = "Subnet group for Redis cluster"
+}
+
+resource "aws_security_group" "redis" {
+  name        = "security-group-redis"
+  description = "Allow access to Redis"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = var.ingress_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "innfis-redis"
+  engine               = "redis"
+  node_type            = var.node_type # "cache.t3.micro"
+  num_cache_nodes      = 2
+  parameter_group_name = var.parameter_group # "default.redis7"
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.redis.name
+  security_group_ids   = [aws_security_group.redis.id]
 }
