9d54f27d56a841ccd0a602a2cf885fe00ede1e5c
innfi <innfi.world@gmail.com>
2025-06-02 22:48:33 +0900

    tiny-mlops: back to the beginning

--- compound-exercise/tiny-mlops/modules/storage/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_efs_file_system" "storage" {
-  creation_token = "storage"  
-
-  lifecycle_policy {
-    transition_to_ia = "AFTER_365_DAYS"
-  }
-}
--- compound-exercise/tiny-mlops/modules/storage/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "module_storage_output" {
-  value = aws_efs_file_system.storage.*
-}
