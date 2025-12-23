6aea14be064921c1e287def270eaca64a6c8da12
innfi <innfi.world@gmail.com>
2025-12-23 23:23:13 +0900

    test-cilium: update helm params

--- eks/test-cilium/install_cilium.sh
+++ eks/test-cilium/install_cilium.sh
@@ -4,9 +4,16 @@ helm repo add cilium https://helm.cilium.io/
 helm repo update
 
 # Install Cilium for EKS
-helm install cilium cilium/cilium --version 1.16.5 \
+helm install cilium cilium/cilium \
   --namespace kube-system \
   --set eni.enabled=true \
   --set ipam.mode=eni \
   --set egressMasqueradeInterfaces=eth0 \
-  --set routingMode=native
+  --set routingMode=native \
+  --set endpointRoutes.enabled=true \
+  --set hubble.relay.enabled=true \
+  --set hubble.ui.enabled=true \
+  --set prometheus.enabled=true \
+  --set operator.prometheus.enabled=true
+
+# TODO: check implmentations of other cni
