d738ed95cbbfb70e27c96bd5c3416ae7ba1a9397
innfi <innfi.world@gmail.com>
2026-01-04 19:35:55 +0900

    cni-plugin: initial commit

---
+++ cni-plugin/go.mod
@@ -0,0 +1,14 @@
+module test-cni-plugin
+
+go 1.25.5
+
+require (
+	github.com/containernetworking/cni v1.3.0
+	github.com/containernetworking/plugins v1.9.0
+	github.com/vishvananda/netlink v1.3.1
+)
+
+require (
+	github.com/vishvananda/netns v0.0.5 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+)
---
+++ cni-plugin/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+
+	"github.com/containernetworking/cni/pkg/skel"
+	"github.com/containernetworking/cni/pkg/types"
+	current "github.com/containernetworking/cni/pkg/types/100"
+	"github.com/containernetworking/cni/pkg/version"
+	"github.com/containernetworking/plugins/pkg/ns"
+	"github.com/vishvananda/netlink"
+)
+
+type NetConf struct {
+	types.NetConf
+	Subnet string `json:"subnet"`
+	Bridge string `json:"bridge"`
+}
+
+func cmdAdd(args *skel.CmdArgs) error {
+	conf := NetConf{}
+	if err := json.Unmarshal(args.StdinData, &conf); err != nil {
+		return fmt.Errorf("failed to parse config: %v", err)
+	}
+
+	hostVeth := fmt.Sprintf("veth%s", args.ContainerID[:8])
+	containerVeth := args.IfName
+
+	netns, err := ns.GetNS(args.Netns)
+	if err != nil {
+		return fmt.Errorf("failed to open netns: %v", err)
+	}
+	defer netns.Close()
+
+	veth := &netlink.Veth{
+		LinkAttrs: netlink.LinkAttrs{Name: hostVeth},
+		PeerName:  containerVeth,
+	}
+	if err := netlink.LinkAdd(veth); err != nil {
+		return fmt.Errorf("failed to create veth pair: %v", err)
+	}
+
+	containerIface, err := netlink.LinkByName(containerVeth)
+	if err != nil {
+		return err
+	}
+	if err := netlink.LinkSetNsFd(containerIface, int(netns.Fd())); err != nil {
+		return err
+	}
+
+	if err := netns.Do(func(_ ns.NetNS) error {
+		link, err := netlink.LinkByName(containerVeth)
+		if err != nil {
+			return err
+		}
+
+		// TODO: replace this code with ipam
+		addr, _ := netlink.ParseAddr("10.244.0.2/24")
+		if err := netlink.AddrAdd(link, addr); err != nil {
+			return err
+		}
+
+		if err := netlink.LinkSetUp(link); err != nil {
+			return err
+		}
+
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	// set result
+	result := &current.Result{
+		CNIVersion: conf.CNIVersion,
+		Interfaces: []*current.Interface{
+			{Name: containerVeth},
+		},
+		IPs: []*current.IPConfig{
+			{
+				Address: net.IPNet{
+					IP:   net.ParseIP("10.244.0.2"),
+					Mask: net.CIDRMask(24, 32),
+				},
+			},
+		},
+	}
+
+	return types.PrintResult(result, conf.CNIVersion)
+}
+
+func cmdDel(args *skel.CmdArgs) error {
+	hostVeth := fmt.Sprintf("veth%s", args.ContainerID[:8])
+
+	link, err := netlink.LinkByName(hostVeth)
+	if err != nil {
+		return err
+	}
+
+	return netlink.LinkDel(link)
+}
+
+func cmdCheck(args *skel.CmdArgs) error {
+	return nil
+}
+
+func main() {
+	skel.PluginMainFuncs(skel.CNIFuncs{
+		Add:   cmdAdd,
+		Del:   cmdDel,
+		Check: cmdCheck,
+	}, version.All, "test-cni v0.0.1")
+}
