30e14c7f331028d7d9ed3e9af922737a4d61e47e
innfi <innfi.world@gmail.com>
2026-01-07 23:15:22 +0900

    test-cilium: policy for ebs

--- eks/test-cilium/modules/pod-identity-association/main.tf
+++ eks/test-cilium/modules/pod-identity-association/main.tf
@@ -30,3 +30,21 @@ resource "aws_eks_pod_identity_association" "s3_access_association" {
   service_account = var.service_account
   role_arn     = aws_iam_role.s3_access.arn
 }
+
+# TODO: merge with s3 access?
+resource "aws_iam_role" "ebs_access" {
+  name = "eks-pod-identity-ebs-access"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_access_attachment" {
+  role = aws_iam_role.ebs_access
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+resource "aws_eks_pod_identity_association" "ebs_access_association" {
+  cluster_name = var.cluster_name
+  namespace    = var.namespace
+  service_account = var.service_account
+  role_arn     = aws_iam_role.ebs_access.arn
+}
