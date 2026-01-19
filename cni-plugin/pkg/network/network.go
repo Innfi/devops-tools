5ef22cec3b1dc088ae1ba465bb3007bd33d2b852
innfi <innfi.world@gmail.com>
2026-01-19 23:06:43 +0900

    cni-plugin: start packaging

--- cni-plugin/pkg/network/network.go
+++ cni-plugin/pkg/network/network.go
@@ -1,11 +1,26 @@
 package network
 
-// import (
-// 	"test-cni-plugin/pkg/iptableswrapper"
-// 	"test-cni-plugin/pkg/netlinkwrapper"
-// 	"test-cni-plugin/pkg/nswrapper"
-// )
+import (
+	"test-cni-plugin/pkg/iptableswrapper"
+	"test-cni-plugin/pkg/netlinkwrapper"
+	"test-cni-plugin/pkg/nswrapper"
+	"test-cni-plugin/pkg/vethwrapper"
+)
 
-// TODO: how to define the role and the responsibility
 type Network struct {
+	netlink  netlinkwrapper.NetLink
+	ns       nswrapper.NS
+	veth     vethwrapper.Veth
+	iptables iptableswrapper.IPTablesIface
 }
+
+/* TODO
+* func (n *Network) SetupContainerNetwork(args *skel.CmdArgs, conf *config.NetConf) (*current.Result, error) {
+    // Orchestrate all operations
+}
+
+func (n *Network) TeardownContainerNetwork(args *skel.CmdArgs) error {
+    // Clean teardown
+}
+*
+*/
